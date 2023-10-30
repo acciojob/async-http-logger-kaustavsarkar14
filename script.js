0e0a50e458bba0e05168f0aded7ae0b5105c75a6
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-10-30 10:57:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+async function getData(){
+	const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+	const data = await res.json()
+	console.log(data)
+}
+getData()
